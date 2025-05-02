416ece8633cf3e08876d933f781fdddb21cddf21
morrs <grishkovea@gmail.com>
2025-05-02 16:11:37 +0300

    добавил список файлов для отображения

---
+++ src/data/products.js
@@ -0,0 +1,34 @@
+export const products = [
+    {
+        id: 1,
+        name: "Горный велосипед X-Trail",
+        category: "Горные",
+        price: 24990,
+        rating: 4.5,
+        image: "mountain-bike.jpg"
+    },
+    {
+        id: 2,
+        name: "Шоссейный велосипед SpeedPro",
+        category: "Шоссейные",
+        price: 31990,
+        rating: 4.7,
+        image: "road-bike.jpg"
+    },
+    {
+        id: 3,
+        name: "Детский велосипед KidCycle",
+        category: "Детские",
+        price: 8990,
+        rating: 4.2,
+        image: "kids-bike.jpg"
+    },
+    {
+        id: 4,
+        name: "Электровелосипед EcoRide",
+        category: "Электровелосипеды",
+        price: 54990,
+        rating: 4.8,
+        image: "electric-bike.jpg"
+    }
+];
